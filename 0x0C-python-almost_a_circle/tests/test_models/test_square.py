de20291077a22a5ccfed4221ff9abb8bfa4b1d96
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-04-17 19:38:39 +0100

    Python - almost a circle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+""" Module for test Square class """
+import unittest
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+from models.square import Square
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestSquareMethods(unittest.TestCase):
+    """ Suite to test Square class """
+
+    def setUp(self):
+        """ Method invoked for each test """
+        Base._Base__nb_objects = 0
+
+    def test_new_square(self):
+        """ Test new square """
+        new = Square(3)
+        self.assertEqual(new.size, 3)
+        self.assertEqual(new.width, 3)
+        self.assertEqual(new.height, 3)
+        self.assertEqual(new.x, 0)
+        self.assertEqual(new.y, 0)
+        self.assertEqual(new.id, 1)
+
+    def test_new_square_2(self):
+        """ Test new square with all attrs """
+        new = Square(2, 5, 5, 4)
+        self.assertEqual(new.size, 2)
+        self.assertEqual(new.width, 2)
+        self.assertEqual(new.height, 2)
+        self.assertEqual(new.x, 5)
+        self.assertEqual(new.y, 5)
+        self.assertEqual(new.id, 4)
+
+    def test_new_squares(self):
+        """ Test new squares """
+        new = Square(1, 1)
+        new2 = Square(1, 1)
+        self.assertEqual(False, new is new2)
+        self.assertEqual(False, new.id == new2.id)
+
+    def test_is_Base_instance(self):
+        """ Test Square is a Base instance """
+        new = Square(1)
+        self.assertEqual(True, isinstance(new, Rectangle))
+
+    def test_incorrect_amount_attrs(self):
+        """ Test error raise with no args passed """
+        with self.assertRaises(TypeError):
+            new = Square()
+
+    def test_incorrect_amount_attrs_1(self):
+        """ Test error raised with no args passed """
+        with self.assertRaises(TypeError):
+            new = Square(1, 1, 1, 1, 1)
+
+    def test_access_private_attrs(self):
+        """ Trying to access to a private attribute """
+        new = Square(1)
+        with self.assertRaises(AttributeError):
+            new.__width
+
+    def test_access_private_attrs_2(self):
+        """ Trying to access to a private attribute """
+        new = Square(1)
+        with self.assertRaises(AttributeError):
+            new.__height
+
+    def test_access_private_attrs_3(self):
+        """ Trying to access to a private attribute """
+        new = Square(1)
+        with self.assertRaises(AttributeError):
+            new.__x
+
+    def test_access_private_attrs_4(self):
+        """ Trying to access to a private attribute """
+        new = Square(1)
+        with self.assertRaises(AttributeError):
+            new.__y
