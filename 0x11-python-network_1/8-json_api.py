f38261a5c251657fe444932093b0ba112039ae91
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2023-07-02 19:49:19 +0100

    corrections made

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,26 @@
-#!/usr/bin/python3i
-"""A script that:
-- takes in a letter
-- sends POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
-"""
-import sys
+#!/usr/bin/python3
+'''module:
+sends a POST request to search for a user based on a letter(character) param
+and displays the results.
+'''
+
 import requests
+import sys
+
+msg = ['No result', 'Not a valid JSON']
 
 
-if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+if __name__ == '__main__':
+    url = 'http://0.0.0.0:5000/search_user'
+    char = sys.argv[1] if len(sys.argv) > 1 else ''
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    res = requests.post(url, data={'q': char})
+
     try:
-        response = r.json()
-        if response == {}:
-            print("No result")
+        data = res.json()
+        if data:
+            print(f"[{data['id']}] {data['name']}")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print(msg[0])
     except ValueError:
-        print("Not a valid JSON")
+        print(msg[1])
